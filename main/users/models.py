6a98ef55df53a3e7fa92c67ebe3bcb732f79f307
kirkylesich <stealer-mail@mail.ru>
2021-09-04 00:36:29 +0500

    new

--- main/users/models.py
+++ main/users/models.py
@@ -3,4 +3,18 @@ from django.contrib.auth.models import User
 
 
 
+class OpUser(models.Model):
+    mail = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    score = models.IntegerField()
+    money = models.IntegerField()
+    token = models.CharField(max_length=255)
 
+    def create(self):
+        pass
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=250)
+    user = models.ForeignKey("OpUser", on_delete=models.CASCADE)
